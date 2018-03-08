78e857095346f60e4efe653042b543fce7bba0ae
Adam Flanagan <adam.flanagan@skybettingandgaming.com>
2018-03-08 10:58:38 +0000

    [M-1] Simple question and answer func with marker func

--- src/main.go
+++ src/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+)
+// func main() {
+//     fmt.Print("Enter text: ")
+//     var input string
+//     // fmt.Scanln(&input)
+//     fmt.Print(input)
+// }
+func Marker (selection string, answer string) (result bool) {
+    if (selection == answer) {
+        result = true
+    } else {
+        result = false
+    }
+    return
+}
 
-func main () {
-    fmt.Println("Staring up!")
+func PrintQuestion (question string, rightAnswer string, options ...string) (answer string, correctAnswer string) {
+    fmt.Println("<<" + question + ">>")
+    fmt.Println("<" + options[0] + ">" + " " + "<" + options[1] + ">")
+    fmt.Println("<" + options[2] + ">" + " " + "<" + options[3] + ">")
+    var input string
+    fmt.Scanln(&input)
+    answer = input
+    correctAnswer = rightAnswer
+    return
+}
+
+func main() {
+    answer, correctAnswer := PrintQuestion("What is the letter a?", "a", "a", "b", "c", "d")
+    result := Marker(answer, correctAnswer)
+    fmt.Println(result)
 }
